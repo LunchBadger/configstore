a9210e02151235591794b3132583948bd6ea0d6a
Roman Lisagor <rlisagor@gmail.com>
2016-06-27 11:21:05 -0700

    No longer using PersistedModel as the base class for the models.
    Originally, I thought that this would allow for easier exposure through the REST adapter. However, the PersistedModel class doesn't expose relations as REST methods unless a data source is defined. In addition, none of the higher-level functions in PersistedModel (e.g. change streams) seem to work unless you have a data suorce defined as well. So, there's not much benefit to using PersistedModel as a base class.

--- server/models/repo.js
+++ server/models/repo.js
@@ -4,7 +4,6 @@ let Promise = require('bluebird');
 let gitrepo = require('../lib/gitrepo');
 let error = require('../lib/error');
 
-
 module.exports = function(Repo) {
 
   Repo.create = function(data) {
@@ -19,7 +18,7 @@ module.exports = function(Repo) {
     return this.manager.repoExists(id);
   };
 
-  Repo.findById = function(id) {
+  Repo.getOne = function(id) {
     return this.manager.getRepo(id)
       .then(repo => { return {id: repo.name} })
       .catch(gitrepo.RepoDoesNotExistError, (err) => {
@@ -27,7 +26,7 @@ module.exports = function(Repo) {
       });
   };
 
-  Repo.find = function() {
+  Repo.getAll = function() {
     return this.manager.getAllRepos().then((repos) => {
       return repos.map((repo) => { return {id: repo.name}; });
     });
@@ -39,10 +38,42 @@ module.exports = function(Repo) {
     });
   };
 
-  Repo.disableRemoteMethod('upsert', true);
-  Repo.disableRemoteMethod('updateAll', true);
-  Repo.disableRemoteMethod('findOne', true);
-  Repo.disableRemoteMethod('createChangeStream', true);
-  Repo.disableRemoteMethod('count', true);
-  Repo.disableRemoteMethod('updateAttributes', false);
+  Repo.remoteMethod('create', {
+    description: 'Create a new repository.',
+    accepts: [{arg: 'repo', type: Repo, http: {source: 'body'}}],
+    returns: [{arg: 'repo', type: Repo, root: true}],
+    http: {verb: 'post', path: '/'}
+  });
+
+  Repo.remoteMethod('exists', {
+    description: 'Check whether a repository exists.',
+    accepts: [
+      {arg: 'name', type: 'string', description: 'Repo name', required: true}
+    ],
+    returns: {arg: 'exists', type: 'boolean'},
+    http: {verb: 'get', path: '/:id/exists'}
+  });
+
+  Repo.remoteMethod('getOne', {
+    description: 'Retrieve the information for the given repo.',
+    accepts: [
+      {arg: 'id', type: 'string', description: 'Repo id', required: true}
+    ],
+    returns: {arg: 'data', type: Repo, root: true},
+    http: {verb: 'get', path: '/:id'}
+  });
+
+  Repo.remoteMethod('getAll', {
+    description: 'Retrieve information on all existing repos.',
+    returns: {arg: 'data', type: [Repo], root: true},
+    http: {verb: 'get', path: '/'}
+  });
+
+  Repo.remoteMethod('delete', {
+    description: 'Delete a repo.',
+    accepts: {arg: 'id', type: 'any', description: 'Repo id', required: true},
+    http: {verb: 'del', path: '/:id'},
+    returns: {arg: 'count', type: 'object', root: true}
+  });
+
 };
