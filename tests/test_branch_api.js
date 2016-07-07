79ac26da5bfe7895c8e93a478834d689e125cb5a
Roman Lisagor <rlisagor@gmail.com>
2016-07-06 17:19:32 -0700

    Converted branch tests to async/await

--- tests/test_branch_api.js
+++ tests/test_branch_api.js
@@ -9,216 +9,181 @@ describe('Branch API', function() {
   let manager = new RepoManager(app.get('lunchBadger').repoPath);
   let testRepo = null;
 
-  beforeEach(function() {
-    return manager.createRepo('test-config').then((repo) => {
-      testRepo = repo;
-    });
+  beforeEach(async function() {
+    let repo = await manager.createRepo('test-config');
+    testRepo = repo;
   });
 
-  afterEach(function() {
+  afterEach(async function() {
     testRepo = null;
-    return manager.removeAllRepos();
+    await manager.removeAllRepos();
   });
 
-  function createNewBranch(branchName) {
+  async function createNewBranch(branchName) {
     let data = {
       fileA: 'A bunch of configuration',
       fileB: 'Some more configuration'
     };
-    return supertest(app)
+    return await supertest(app)
       .patch(`/api/repos/test-config/branches/${branchName}/files`)
       .send(data)
       .expect(204);
   }
 
-  it('should be able to create a new branch', function() {
-    return createNewBranch('new-branch').then((res) => {
-      assert.isString(res.headers['etag']);
-      assert.notEqual(res.headers['etag'], 'undefined');
-    });
+  it('should be able to create a new branch', async function() {
+    let res = await createNewBranch('new-branch');
+    assert.isString(res.headers['etag']);
+    assert.notEqual(res.headers['etag'], 'undefined');
   });
 
   describe('with an existing branch', function() {
     let revision = null;
 
-    beforeEach(function() {
-      return createNewBranch('my-branch')
-        .then(res => {
-          revision = res.get('ETag');
-          return res;
-        });
+    beforeEach(async function() {
+      let res = await createNewBranch('my-branch');
+      revision = res.get('ETag');
     });
 
     describe('uploading and downloading files', function() {
-      it('should work from the branch', function() {
-        return supertest(app)
+      it('should work from the branch', async function() {
+        let res = await supertest(app)
           .get('/api/repos/test-config/branches/my-branch/files/fileA')
           .expect(200)
-          .expect('A bunch of configuration')
-          .then(res => {
-            assert.deepProperty(res, 'headers.content-type');
-            assert(res.headers['content-type'].startsWith(
-              'application/octet-stream'));
-          });
+          .expect('A bunch of configuration');
+        assert.deepProperty(res, 'headers.content-type');
+        assert(res.headers['content-type'].startsWith(
+            'application/octet-stream'));
       });
 
       it('should return error when trying to download a non-existent file',
-        function() {
-          return supertest(app)
+        async function() {
+          await supertest(app)
             .get('/api/repos/test-config/branches/my-branch/files/fakeFile')
             .expect(404);
         });
 
-      it('should be able to add a new file and read it back', function() {
-        let newRevision = null;
+      it('should be able to add a new file and read it back', async function() {
+        let addRes = await supertest(app)
+          .patch('/api/repos/test-config/branches/my-branch/files')
+          .send({fileC: 'This is my new file'})
+          .set('If-Match', revision)
+          .expect(204);
 
-        function upload() {
-          return supertest(app)
-            .patch('/api/repos/test-config/branches/my-branch/files')
-            .send({fileC: 'This is my new file'})
-            .set('If-Match', revision)
-            .expect(204)
-            .then(res => {
-              newRevision = res.headers.etag;
-            });
-        }
-
-        function check() {
-          return supertest(app)
+        let checkRes = await supertest(app)
             .get('/api/repos/test-config/branches/my-branch/files/fileC')
             .expect(200)
-            .expect('This is my new file')
-            .then(res => {
-              assert.equal(res.header['etag'], newRevision);
-            });
-        }
-        return upload().then(check);
+            .expect('This is my new file');
+
+        assert.equal(checkRes.header['etag'], addRes.headers.etag);
       });
 
-      it('should reject an update of a bad revision', function() {
-        return supertest(app)
+      it('should reject an update of a bad revision', async function() {
+        await supertest(app)
           .patch('/api/repos/test-config/branches/my-branch/files')
           .send({fileC: 'This is my new file'})
           .set('If-Match', 'incorrect-revision')
           .expect(412);
       });
 
-      it('should reject an update if revision is not passed in', function() {
-        return supertest(app)
-          .patch('/api/repos/test-config/branches/my-branch/files')
-          .send({fileC: 'This is my new file'})
-          .expect(412);
-      });
-
-      it('should be able to modify a file', function() {
-        function update() {
-          return supertest(app)
+      it('should reject an update if revision is not passed in',
+        async function() {
+          await supertest(app)
             .patch('/api/repos/test-config/branches/my-branch/files')
-            .send({fileA: 'Updated file'})
-            .set('If-Match', revision)
-            .expect(204);
+            .send({fileC: 'This is my new file'})
+            .expect(412);
         }
+      );
 
-        function check() {
-          return supertest(app)
-            .get('/api/repos/test-config/branches/my-branch/files/fileA')
-            .expect(200)
-            .expect('Updated file');
-        }
+      it('should be able to modify a file', async function() {
+        await supertest(app)
+          .patch('/api/repos/test-config/branches/my-branch/files')
+          .send({fileA: 'Updated file'})
+          .set('If-Match', revision)
+          .expect(204);
 
-        return update().then(check);
+        await supertest(app)
+          .get('/api/repos/test-config/branches/my-branch/files/fileA')
+          .expect(200)
+          .expect('Updated file');
       });
 
-      it('should error when supplying no data', function() {
-        return supertest(app)
+      it('should error when supplying no data', async function() {
+        await supertest(app)
           .patch('/api/repos/test-config/branches/my-branch/files')
           .set('If-Match', revision)
           .expect(400);
       });
     });
 
-    it('should be able to copy the branch', function() {
-      function copy() {
-        return supertest(app)
-          .put('/api/repos/test-config/branches/second-branch')
-          .send({
-            revision: 'my-branch'
-          })
-          .expect(200);
-      }
+    it('should be able to copy the branch', async function() {
+      let newBranch = await supertest(app)
+        .put('/api/repos/test-config/branches/second-branch')
+        .send({
+          revision: 'my-branch'
+        })
+        .expect(200);
 
-      function check(newBranch) {
-        return Promise.all([
-          supertest(app)
-            .get('/api/repos/test-config/branches/second-branch/files/fileA')
-            .expect(200, 'A bunch of configuration'),
-          supertest(app)
-            .get('/api/repos/test-config/branches/my-branch')
-            .expect(200)
-            .then((oldBranch) => {
-              assert.equal(newBranch.revision, oldBranch.revision);
-            })
-        ]);
-      }
+      await supertest(app)
+        .get('/api/repos/test-config/branches/second-branch/files/fileA')
+        .expect(200, 'A bunch of configuration');
 
-      return copy().then(check);
+      await supertest(app)
+        .get('/api/repos/test-config/branches/my-branch')
+        .expect(200)
+        .then((oldBranch) => {
+          assert.equal(newBranch.revision, oldBranch.revision);
+        });
     });
 
     it('should return an error when trying to copy a non-existent branch',
-      function() {
-        return supertest(app)
+      async function() {
+        await supertest(app)
           .put('/api/repos/test-config/branches/second-branch')
           .send({
             revision: 'does-not-exist'
           })
           .expect(400);
-      });
+      }
+    );
 
-    it('should be able to get the current revision of the branch', function() {
-      return supertest(app)
-        .get('/api/repos/test-config/branches/my-branch')
-        .expect(200)
-        .then(res => {
-          assert.propertyVal(res.body, 'id', 'my-branch');
-          assert.property(res.body, 'revision');
-        });
-    });
+    it('should be able to get the current revision of the branch',
+      async function() {
+        let res = await supertest(app)
+          .get('/api/repos/test-config/branches/my-branch')
+          .expect(200);
 
-    it('should return an error when getting a bad branch', function() {
-      return supertest(app)
+        assert.propertyVal(res.body, 'id', 'my-branch');
+        assert.property(res.body, 'revision');
+      }
+    );
+
+    it('should return an error when getting a bad branch', async function() {
+      await supertest(app)
         .get('/api/repos/test-config/branches/does-not-exist')
         .expect(404);
     });
 
-    it('should be able to delete the branch', function() {
-      function del() {
-        return supertest(app)
-          .del('/api/repos/test-config/branches/my-branch')
-          .expect(200);
-      }
+    it('should be able to delete the branch', async function() {
+      await supertest(app)
+        .del('/api/repos/test-config/branches/my-branch')
+        .expect(200);
 
-      function check() {
-        return supertest(app)
-          .get('/api/repos/test-config/branches/my-branch')
-          .expect(404);
-      }
-
-      return del().then(check);
+      await supertest(app)
+        .get('/api/repos/test-config/branches/my-branch')
+        .expect(404);
     });
 
-    it('should return an error when deleting a bad branch', function() {
-      return supertest(app)
+    it('should return an error when deleting a bad branch', async function() {
+      await supertest(app)
         .del('/api/repos/test-config/branches/does-not-exist')
         .expect(404);
     });
 
-    it('should be able to get all branches in the repo', function() {
-      return supertest(app)
-        .get('/api/repos/test-config')
-        .then(res => {
-          assert.property(res.body, 'branches');
-          assert.deepEqual(res.body.branches, ['my-branch']);
-        });
+    it('should be able to get all branches in the repo', async function() {
+      let res = await supertest(app)
+        .get('/api/repos/test-config');
+      assert.property(res.body, 'branches');
+      assert.deepEqual(res.body.branches, ['my-branch']);
     });
   });
 });
