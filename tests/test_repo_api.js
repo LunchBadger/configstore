fc0e3bca047523747f89d6ec311e9b4c00e7b155
Roman Lisagor <rlisagor@gmail.com>
2016-07-06 17:38:27 -0700

    Converted repo tests to async/await

--- tests/test_repo_api.js
+++ tests/test_repo_api.js
@@ -9,61 +9,51 @@ describe('Repository API', function() {
   let client = supertest(app);
   let manager = new RepoManager(app.get('lunchBadger').repoPath);
 
-  afterEach(function() {
-    return manager.removeAllRepos();
+  afterEach(async function() {
+    await manager.removeAllRepos();
   });
 
-  it('should be able to to create a repo', function() {
-    function create() {
-      return client
-        .post('/api/repos/')
-        .send({id: 'another-item'})
-        .expect(200, {id: 'another-item'});
+  it('should be able to to create a repo', async function() {
+    await client
+      .post('/api/repos/')
+      .send({id: 'another-item'})
+      .expect(200, {id: 'another-item'});
+
+    await client
+      .get('/api/repos/another-item')
+      .expect(200, {id: 'another-item', branches: []});
+  });
+
+  it('should be able to tell you when a repository doesn\'t exist',
+    async function() {
+      await client
+        .get('/api/repos/test-config/exists')
+        .expect(200, {exists: false});
     }
-
-    function check() {
-      return client
-        .get('/api/repos/another-item')
-        .expect(200, {id: 'another-item', branches: []});
-    }
-
-    return create().then(check);
-  });
-
-  it('should be able to tell you when a repository doesn\'t exist', function() {
-    return client
-      .get('/api/repos/test-config/exists')
-      .expect(200, {exists: false});
-  });
+  );
 
   describe('with existing empty repo', function() {
-    beforeEach(function() {
-      return client
+    beforeEach(async function() {
+      await client
         .post('/api/repos/')
         .send({id: 'test-config'})
         .expect(200);
     });
 
-    it('should be able to tell that the repository exists', function() {
-      return client
+    it('should be able to tell that the repository exists', async function() {
+      await client
         .get('/api/repos/test-config/exists')
         .expect(200, {exists: true});
     });
 
-    it('should be able to delete the repo', function() {
-      function del() {
-        return client
-          .del('/api/repos/test-config')
-          .expect(200, {count: 1});
-      }
+    it('should be able to delete the repo', async function() {
+      await client
+        .del('/api/repos/test-config')
+        .expect(200, {count: 1});
 
-      function check() {
-        return client
-          .get('/api/repos/test-config')
-          .expect(404);
-      }
-
-      return del().then(check);
+      await client
+        .get('/api/repos/test-config')
+        .expect(404);
     });
   });
 });
