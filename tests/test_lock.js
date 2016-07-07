25eeb553ddfced013c8199eb6a98d54bc548b51c
Roman Lisagor <rlisagor@gmail.com>
2016-07-06 17:36:15 -0700

    Converted lock tests to async/await

--- tests/test_lock.js
+++ tests/test_lock.js
@@ -8,55 +8,50 @@ chai.use(chaiAsPromised);
 let assert = chai.assert;
 
 describe('File locking system', function() {
-  it('should call provided function', function() {
+  it('should call provided function', async function() {
     let testFn = sinon.spy();
-    return lock('/tmp/testlock', testFn)
-      .then(() => {
-        assert(testFn.called);
-      });
+    await lock('/tmp/testlock', testFn);
+    assert(testFn.called);
   });
 
-  it('should unlock after calling the function', function() {
+  it('should unlock after calling the function', async function() {
     let testFn = sinon.spy();
-    return lock('/tmp/testlock', testFn)
-      .then(() => lock('/tmp/testlock', testFn))
-      .then(() => {
-        assert(testFn.calledTwice);
-      });
+    await lock('/tmp/testlock', testFn);
+    await lock('/tmp/testlock', testFn);
+    assert(testFn.calledTwice);
   });
 
-  it('should propagate the return value of the function', function() {
+  it('should propagate the return value of the function', async function() {
     let testFn = sinon.stub();
     testFn.returns('magic value');
-    return lock('/tmp/testlock', testFn)
-      .then(res => {
-        assert.equal(res, 'magic value');
-      });
+    let res = await lock('/tmp/testlock', testFn);
+    assert.equal(res, 'magic value');
   });
 
-  it('should raise an error if already locked', function() {
+  it('should raise an error if already locked', async function() {
     let testFn = sinon.spy();
     let promise = lock('/tmp/testlock', () => {
       return lock('/tmp/testlock', testFn);
     });
 
-    return assert.isRejected(promise, lock.LockedError)
-      .then(() => {
-        assert(testFn.notCalled);
-      });
+    await assert.isRejected(promise, lock.LockedError);
+    assert(testFn.notCalled);
   });
 
-  it('should propagate + unlock if the function throws an error', function() {
-    function testFn() {
-      throw Error('Uh oh!');
+  it('should propagate + unlock if the function throws an error',
+    async function() {
+      function testFn() {
+        throw Error('Uh oh!');
+      }
+      let testFn2 = sinon.spy();
+
+      let promise = (async () => {
+        await lock('/tmp/testlock', testFn);
+        await lock('/tmp/testlock', testFn2);
+      })();
+
+      await assert.isRejected(promise, Error, 'Uh oh!');
+      assert(testFn2.notCalled);
     }
-    let testFn2 = sinon.spy();
-
-    let promise = lock('/tmp/testlock', testFn)
-      .then(() => lock('/tmp/testlock', testFn2));
-    return assert.isRejected(promise, Error, 'Uh oh!')
-      .then(() => {
-        assert(testFn2.notCalled);
-      });
-  });
+  );
 });
