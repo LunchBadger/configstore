5c9520e006fb737846334c0553a195df986a02b6
Roman Lisagor <rlisagor@gmail.com>
2017-01-31 17:25:02 -0800

    New command ("start:test") that both runs in production mode and creates a demo repo

--- scripts/configureRepo.js
+++ scripts/configureRepo.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const path = require('path');
 const crypto = require('crypto');
 const configureRepoForHttp = require('../server/lib/githttp').configureRepo;
 
-configureRepoForHttp(path.join(__dirname, '../example/repos/demo.git'),
-                     crypto.randomBytes(16).toString('hex'));
+configureRepoForHttp(process.argv[2], crypto.randomBytes(16).toString('hex'));
--- scripts/createRepo.sh
+++ scripts/createRepo.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-DEMODIR=example/repos/demo.git
+DEMODIR=${1:-example/repos/demo.git}
 
 if [ ! -e $DEMODIR ]; then
   mkdir -p $DEMODIR
   pushd $DEMODIR > /dev/null
   git init
   popd > /dev/null
-  PATH=$(npm bin):$PATH babel-node scripts/configureRepo.js
+  PATH=$(npm bin):$PATH babel-node scripts/configureRepo.js $DEMODIR
 fi
