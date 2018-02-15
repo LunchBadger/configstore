db7c6820f2afcaedce48f6ddeb70467844ae9a12
DrMegavolt <sergeykuc@gmail.com>
2018-02-16 01:01:58 +0200

    hook subscription + SSE

---
+++ hook-example.js
@@ -0,0 +1,68 @@
+// eslint-disable-next-line
+let a = {
+  secret: '',
+  ref: 'refs/heads/master',
+  before: '559fbe074a3f12100717b906e52663f95deeff5d',
+  after: 'c04138d152d24e39cc7efef00b9635be2bf8e539',
+  compare_url: 'http://gitea.local.io/customer-zu4/dev/compare/559fbe074a3f12100717b906e52663f95deeff5d...c04138d152d24e39cc7efef00b9635be2bf8e539',
+  commits:
+  [{
+    id: 'c04138d152d24e39cc7efef00b9635be2bf8e539',
+    message: 'Update \'package.json\'\n',
+    url: 'http://gitea.local.io/customer-zu4/dev/commit/c04138d152d24e39cc7efef00b9635be2bf8e539',
+    verification: null,
+    timestamp: '2018-02-15T22:23:52Z'
+  }],
+  repository:
+  {
+    id: 4,
+    owner:
+    {
+      id: 8,
+      login: 'customer-zu4',
+      full_name: '',
+      email: 'zu4@customer.com',
+      avatar_url: 'https://secure.gravatar.com/avatar/e0953992ad51bacfc436fee8ad2d0ac4?d=identicon',
+      username: 'customer-zu4'
+    },
+    name: 'dev',
+    full_name: 'customer-zu4/dev',
+    description: '',
+    empty: false,
+    private: false,
+    fork: false,
+    parent: null,
+    mirror: false,
+    size: 33,
+    html_url: 'http://gitea.local.io/customer-zu4/dev',
+    ssh_url: 'git@gitea.local.io:customer-zu4/dev.git',
+    clone_url: 'http://gitea.local.io/customer-zu4/dev.git',
+    website: '',
+    stars_count: 0,
+    forks_count: 0,
+    watchers_count: 1,
+    open_issues_count: 0,
+    default_branch: 'master',
+    created_at: '2018-02-15T12:46:08Z',
+    updated_at: '2018-02-15T22:23:53Z',
+    permissions: { admin: false, push: false, pull: false }
+  },
+  pusher:
+  {
+    id: 1,
+    login: 'drmegavolt',
+    full_name: '',
+    email: 'sergeykuc@gmail.com',
+    avatar_url: 'https://secure.gravatar.com/avatar/97be4c3cd79211ca42f908bb51837d7a?d=identicon',
+    username: 'drmegavolt'
+  },
+  sender:
+  {
+    id: 1,
+    login: 'drmegavolt',
+    full_name: '',
+    email: 'sergeykuc@gmail.com',
+    avatar_url: 'https://secure.gravatar.com/avatar/97be4c3cd79211ca42f908bb51837d7a?d=identicon',
+    username: 'drmegavolt'
+  }
+};
--- index.js
+++ index.js
@@ -1,130 +1,156 @@
 const express = require('express');
 const request = require('superagent');
-const debug = require('debug')("configstore:")
-const cors = require('cors')
+const debug = require('debug')('configstore:');
+const SseChannel = require('sse-channel');
+const cors = require('cors');
+const channels = {};
 const app = express();
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  debug('Unhandled Rejection at: Promise', p, 'reason:', reason);
     // application specific logging, throwing an error, or other logic here
 });
 
-baseUrl = require('superagent-prefix')(process.env.GIT_API_URL || 'http://localhost:8080');
+let baseUrl = require('superagent-prefix')(process.env.GIT_API_URL || 'http://localhost:8080');
 app.use(express.json());
-app.use(cors({
-    origin: true,
-    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ["Cache-Control", "Content-Type", "Accept", "Authorization", "Accept-Encoding", "Access-Control-Request-Headers", "User-Agent", "Access-Control-Request-Method", "Pragma", "Connection", "Host"],
-    credentials: true
-}))
-const prefix = process.env.REPO_PREFIX || 'customer'
+app.use('/producers', cors({
+  origin: true,
+  methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Cache-Control', 'Content-Type', 'Accept', 'Authorization', 'Accept-Encoding', 'Access-Control-Request-Headers', 'User-Agent', 'Access-Control-Request-Method', 'Pragma', 'Connection', 'Host'],
+  credentials: true
+}));
+const prefix = process.env.REPO_PREFIX || 'customer';
+
+const ensureChannel = (user) => {
+    // TODO double cors
+  channels[user] = channels[user] || new SseChannel({});
+  return channels[user];
+};
+
+app.post('/hook', (req, res) => {
+  res.json({ ok: true }); // no need to hold hook connection
+
+  let { ref, before, after } = req.body;
+  let [namespace, username] = req.body.repository.username.split('-');
+  let payload = { ref, before, after, namespace, username };
+  payload.repoName = req.body.repository.name;
+  let ch = ensureChannel(username);
+  ch.send({ data: JSON.stringify(payload) });
+});
+
+app.get('/change-stream/:user', (req, res) => {
+  ensureChannel(req.params.user).addClient(req, res);
+});
 app.post('/producers', async (req, res) => {
     // 1) Ensure user exists
     // 2) ensure repo `dev` exists
     // 3) ensure repo `functions` exists
+    // 4) ensure Web hook exists TODO
 
-    // Step 1 
-    let user = await findUser(req.body.id)
-    if (!user) {
-        debug(`user not found, creating ${prefix}-${req.body.id}`)
-        user = await request
+    // Step 1
+  let user = await findUser(req.body.id);
+  if (!user) {
+    debug(`user not found, creating ${prefix}-${req.body.id}`);
+    user = await request
             .post('/users')
             .use(baseUrl)
             .send({ name: req.body.id, prefix })
             .then(r => r.body)
             .catch(err => {
-                debug(err)
-                return null
-            })
-    }
-    if (!user) {
-        return res.status(500).json({ "message": "user creation failed" })
-    }
+              debug(err);
+              return null;
+            });
+  }
+  if (!user) {
+    return res.status(500).json({ 'message': 'user creation failed' });
+  }
 
-    repos = await getRepos({ prefix, name: req.body.id })
+  let repos = await getRepos({ prefix, name: req.body.id });
     // Step 2
-    if (repos.every(x => x.name !== "dev")) {
-        await ensureRepo({ repoName: "dev", prefix, name: req.body.id })
-    }
+  if (repos.every(x => x.name !== 'dev')) {
+    await ensureRepo({ repoName: 'dev', prefix, name: req.body.id });
+  }
 
     // Step 3
-    if (repos.every(x => x.name !== "functions")) {
-        await ensureRepo({ repoName: "functions", prefix, name: req.body.id })
-    }
-    repos = await getRepos({ prefix, name: req.body.id })
-    res.json({ id: req.body.id, user, repos })
+  if (repos.every(x => x.name !== 'functions')) {
+    await ensureRepo({ repoName: 'functions', prefix, name: req.body.id });
+  }
+  repos = await getRepos({ prefix, name: req.body.id });
+  res.json({ id: req.body.id, user, repos });
 });
 
 app.get('/producers/:username', async (req, res) => {
-    let user = await findUser(req.params.username)
-    if (!user) {
-        return res.status(404).end()
-    }
-    let repos = await getRepos({ name: req.params.username, prefix })
+  let user = await findUser(req.params.username);
+  if (!user) {
+    return res.status(404).end();
+  }
+  let repos = await getRepos({ name: req.params.username, prefix });
 
-    res.json({ id: req.params.username, envs: {}, user: user.user, repos })
+  res.json({ id: req.params.username, envs: {}, user: user.user, repos });
 });
 
 app.get('/producers/', async (req, res) => {
-    let users = await findUsers(prefix)
-    if (!users) {
-        return res.json({ users: [] })
-    }
-    users = users.filter(u => u.login.indexOf(prefix + '-') >= 0).map(async u => {
-        u.name = u.login.replace(prefix + '-', "")
-        u.namespace = prefix
-        u.repos = await getRepos({ name: u.name, prefix });
-        return u
-    })
-    users = await Promise.all(users)
+  let users = await findUsers(prefix);
+  if (!users) {
+    return res.json({ users: [] });
+  }
+  users = users.filter(u => u.login.indexOf(prefix + '-') >= 0).map(async u => {
+    u.name = u.login.replace(prefix + '-', '');
+    u.namespace = prefix;
+    u.repos = await getRepos({ name: u.name, prefix });
+    return u;
+  });
+  users = await Promise.all(users);
 
-    res.json(users)
+  res.json(users);
 });
 
-async function findUser(name) {
-    return request
+async function findUser (name) {
+  return request
         .get('/users/' + prefix + '/' + name)
         .use(baseUrl)
         .then(r => r.body)
         .catch(err => {
-            return null
-        })
+          debug(err);
+          return null;
+        });
 }
 
-async function findUsers(prefix) {
-    return request
+async function findUsers (prefix) {
+  return request
         .get(`/search/users?q=${prefix}&limit=1000`)
         .use(baseUrl)
         .then(r => r.body.users)
         .catch(err => {
-            return null
-        })
+          debug(err);
+          return null;
+        });
 }
 
-async function ensureRepo({ name, prefix, repoName }) {
-    return request
+async function ensureRepo ({ name, prefix, repoName }) {
+  return request
         .put(`/users/${prefix}/${name}/repos/${repoName}`)
         .use(baseUrl)
-        .then(r => { console.log(r); return r.body })
+        .then(r => r.body)
         .catch(err => {
-            debug(err)
-            return null
-        })
+          debug(err);
+          return null;
+        });
 }
 
-async function getRepos({ name, prefix }) {
-    return request
+async function getRepos ({ name, prefix }) {
+  return request
         .get(`/users/${prefix}/${name}/repos`)
         .use(baseUrl)
         .then(r => r.body.repos)
         .catch(err => {
-            return null
-        })
+          debug(err);
+          return null;
+        });
 }
 
-
 app.all('/producers/:username/accesskey', (req, res) => {
-    res.json({ key: null, message: 'NOT REQUIRED, JUST FOR BACKWARD COMPATIBILITY' })
-})
+  res.json({ key: null, message: 'NOT REQUIRED, JUST FOR BACKWARD COMPATIBILITY' });
+});
 
 app.listen(3002, () => console.log('Configstore API is running port 3002'));
